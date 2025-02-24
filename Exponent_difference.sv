4b29eca7b2e97c62ad020f4984584db28afd7b55
JesusMLastreUMA <jesusmlastre@uma.es>
2025-02-24 08:35:12 +0100

    Modification of exponent difference

--- Exponent_difference.sv
+++ Exponent_difference.sv
@@ -6,12 +6,14 @@ module Exponent_difference #(
     input logic [E-1:0] Ex,
     input logic [E-1:0] Ey,
     output logic signed [E:0] dif,
-    output logic X_greater_than_Y
+    output logic X_greater_than_Y,
+    output logic Ex_equal_Ey
     );
 
 always_comb begin
     dif = {1'b0, Ex} - {1'b0, Ey};
     
     X_greater_than_Y = (dif >= 0);
+    Ex_equal_Ey = (dif == 0);
 end
 endmodule
