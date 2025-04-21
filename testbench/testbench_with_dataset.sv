b6d7d51075355b3765081ac9e8074deb8c317e91
JesusMLastreUMA <jesusmlastre@uma.es>
2025-04-21 10:13:53 +0200

    Translation of comments

--- testbench/testbench_with_dataset.sv
+++ testbench/testbench_with_dataset.sv
@@ -7,15 +7,6 @@ module testbench;
     
   // Señales
   logic [E+M:0] X, Y, Z;
-  logic [M+1:0] result;  // Solo queremos ver la mantisa sumada/restada
-  logic subtraction_output;
-  logic M_major_sign_output;
-  logic [M+2:0] M_major_output;
-  logic [M+2:0] M_minor_output;
-  logic [M+2:0] M_minor_output_C2;
-  logic [E:0] diff_output;
-  logic [E:0] Ez_output;
-  logic [$clog2(M)-1:0] shift_LZA_output;
   
   // Señales para la lectura del dataset
   int file, log_file, r, total_test, correct_test;
@@ -26,28 +17,19 @@ module testbench;
   my_FPHUB_adder #(M, E) dut (
       .X(X),
       .Y(Y),
-      .Z(Z),
-      .result_out(result),  // Conectamos la salida nueva
-      .subtraction_output(subtraction_output),
-      .M_major_sign_output(M_major_sign_output),
-      .M_major_output(M_major_output),
-      .M_minor_output(M_minor_output),
-      .M_minor_output_C2(M_minor_output_C2),
-      .diff_output(diff_output),
-      .Ez_output(Ez_output),
-      .shift_LZA_output(shift_LZA_output)
+      .Z(Z)
   );
 
   initial begin
     // Abrir el archivo CSV con los datos de prueba
-    file = $fopen("additions_4_bit.csv", "r");
+    file = $fopen("hub_float_addition_exp4_mant4.csv", "r");
     if (file == 0) begin
       $display("Error: No se pudo abrir el archivo CSV.");
       $finish;
     end
     
     // Abrir archivo para guardar logs
-    log_file = $fopen("4_bits_log_results_adder_v2.txt", "w");
+    log_file = $fopen("4_bits_log_results_adder_1extra_bits.txt", "w");
     if (log_file == 0) begin
       $display("Error: No se pudo abrir el archivo de logs.");
       $finish;
@@ -77,7 +59,6 @@ module testbench;
             
             // Guardar en el log
             $fdisplay(log_file, "[X] Test %d incorrecto: X(%b) + Y(%b) = %b", total_test, X, Y, Z);
-            $fdisplay(log_file, "Ez normalizado = %b", Ez_output);
             $fdisplay(log_file, "Resultado esperado ---------------> %b \n", expected_result);
           end
         end
@@ -95,10 +76,10 @@ module testbench;
     end
     
     $display("--------------------------------------------------------------");
-    $display("Resultados: %d/%d tests superados", correct_test, total_test);
+    $display("Resultados: %d/%d tests superados", correct_test, total_test-1);
     //$display("--------------------------------------------------------------");
     $fdisplay(log_file, "--------------------------------------------------------------");
-    $fdisplay(log_file, "Resultados finales: %d/%d tests superados", correct_test, total_test);
+    $fdisplay(log_file, "Resultados finales: %d/%d tests superados", correct_test, total_test-1);
     $fdisplay(log_file, "--------------------------------------------------------------");
     
     // Cerrar archivos
